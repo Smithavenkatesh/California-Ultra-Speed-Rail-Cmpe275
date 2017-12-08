3fe1b4a41ae6ab2db5cc16dfae1a1f6eae49a8da
Smitha Venkatesh <Smithavenkatesh@users.noreply.github.com>
2017-12-07 23:55:56 -0800

    Updated LoginForm.java

--- SpringBootHelloWorld/src/main/java/com/javainuse/model/LoginForm.java
+++ SpringBootHelloWorld/src/main/java/com/javainuse/model/LoginForm.java
@@ -5,5 +5,25 @@ import javax.validation.constraints.Size;
 
 public class LoginForm {
     
+    private String email;
 
+  //  @NotNull
+    @Size(min=1, max=50)
+    private String password;
+
+    public String getemail() {
+        return email;
+    }
+
+    public void setUsername(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
