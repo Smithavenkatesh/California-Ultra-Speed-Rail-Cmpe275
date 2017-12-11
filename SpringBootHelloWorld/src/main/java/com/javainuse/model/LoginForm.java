0065d98c8a9b27f37f22ac403927e199c6fcb313
renu-parameswaran <renu.parameswaran@gmail.com>
2017-12-11 13:59:13 -0800

    Refactoring base project structure
    Refactoring base project structure

--- SpringBootHelloWorld/src/main/java/com/javainuse/model/LoginForm.java
+++
@@ -1,9 +0,0 @@
-package com.javainuse.model;
-
-//import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-public class LoginForm {
-    
-
-}
--- SpringBootHelloWorld/src/main/webapp/WEB-INF/jsp/welcome.jsp
+++
@@ -1,3 +0,0 @@
-
-<h1>Welcome to Spring Boot</h1>
-
