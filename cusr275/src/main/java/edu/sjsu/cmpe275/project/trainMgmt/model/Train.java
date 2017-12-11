7c19d7f743d79597fc13c0b1b4c33bd9e558ee4a
Smitha Venkatesh <Smithavenkatesh@users.noreply.github.com>
2017-12-11 15:01:12 -0800

    Added Train Model Object File

--- cusr275/src/main/java/edu/sjsu/cmpe275/project/trainMgmt/model/Train.java
+++ cusr275/src/main/java/edu/sjsu/cmpe275/project/trainMgmt/model/Train.java
@@ -18,5 +18,75 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(allowGetters = true)
 public class Train implements Serializable {
 		
-	//TODO
+		@Id
+		@GeneratedValue(strategy=GenerationType.TABLE)		
+	 	@Column(name="trainID", nullable = false, unique = true)
+	 	private String trainID;
+	 	
+	 	@Column(name="trainType")
+	 	private String trainType;
+	 	
+	 	@Column(name="trainStartTime")
+	 	private String trainStartTime;
+	 	
+	 	@Column(name="trainFareForFiveStn")
+		private String trainFareForFiveStn;
+	 	
+	 	@Column(name="trainEndTime")
+	 	private String trainEndTime;
+
+		/**
+		 * Default Constructor
+		 */
+
+		public Train() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+		
+		
+		/**
+		 * Parameterized Constructor
+		 * @param trainType
+		 * @param trainStartTime
+		 * @param trainEndTime
+		 */
+		public Train(String trainType, String trainStartTime, String trainEndTime) {
+			super();
+			this.trainType = trainType;
+			this.trainStartTime = trainStartTime;
+			this.trainEndTime = trainEndTime;
+		}
+		
+		
+		public String getTrainID() {
+			return trainID;
+		}
+
+
+		public String getTrainType() {
+			return trainType;
+		}
+
+
+		public String getTrainStartTime() {
+			return trainStartTime;
+		}
+
+
+		public String getTrainFareForFiveStn() {
+			return trainFareForFiveStn;
+		}
+		
+		public String getTrainEndTime() {
+			return trainEndTime;
+		}
+		
+		@Override
+		public String toString() {
+			return "Train [trainID=" + trainID + ", trainType=" + trainType + ", trainStartTime=" + trainStartTime
+					+ ", trainFareForFiveStn=" + trainFareForFiveStn + ", trainEndTime=" + trainEndTime + "]";
+		}
+
 }
+
