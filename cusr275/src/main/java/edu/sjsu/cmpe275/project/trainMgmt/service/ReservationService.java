829a81d896fabd551ee80a90e971d55289d7082c
Megha M Raykar <megharaykar@users.noreply.github.com>
2017-12-11 18:25:19 -0800

    Updated ReservationService.java

--- cusr275/src/main/java/edu/sjsu/cmpe275/project/trainMgmt/service/ReservationService.java
+++ cusr275/src/main/java/edu/sjsu/cmpe275/project/trainMgmt/service/ReservationService.java
@@ -19,5 +19,53 @@ import edu.sjsu.cmpe275.project.trainMgmt.model.Reservation;
 @RequestMapping("/v1")
 public class ReservationService {
 
-//TODO
+	@Autowired
+	private ReservationRepository reservationRepository;
+	
+	public ReservationService(ReservationRepository reservationRepository) {
+		this.reservationRepository = reservationRepository;
+		}
+	
+	// Retrieve all rows from table and populate list with objects
+	public List<Object> getAllReservations() {
+		
+		ArrayList<Object> reservations = new ArrayList<Object>();
+		reservationRepository.findAll().forEach(reservations::add);
+		
+		return reservations;
+	}
+	
+	public List<Reservation> findAll() {
+		return reservationRepository.findAll();
+	}
+public Reservation find(Long id){
+		
+		return reservationRepository.findOne(id);
+	}
+	
+
+
+	// Retrieves one row from table based on given id
+//	public Reservation getReservation(String id) {
+//		return reservationRepository.findOne(id);
+//	
+//	}
+	
+	// Inserts row into table 
+	public void addReservation(Reservation Reservation) {
+		reservationRepository.save(Reservation);
+	}
+	
+	
+	public List<Reservation> findReservationByuid(Long uid){
+		return reservationRepository.retrieveUsersByUid(uid);
+	}
+	
+	
+	
+//	// Removes row from table
+//	public void deleteReservation(String id) {
+//		reservationRepository.delete(id);
+//	}
+	
 }
